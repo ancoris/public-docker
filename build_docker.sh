3e0cd0cd8048a6e76617f8f92c075fd822442b21
Chris Gill <cgill@ancoris.com>
2019-07-24 11:13:35 +0100

    ACD-3462: Prefer git branch as tag.

--- build_docker.sh
+++ build_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-timestamp=$(date --utc +%Y-%m-%d_%H-%M-%S_%Z)
+tag=$(git symbolic-ref --short HEAD)
+if [ "${tag}" = "master" ]; then
+    tag=$(date --utc +%Y-%m-%d_%H-%M-%S_%Z)
+fi
 
 docker build appengine --no-cache --tag ancoris/appengine
-docker tag ancoris/appengine ancoris/appengine:${timestamp}
-docker push ancoris/appengine:${timestamp}
+docker tag ancoris/appengine ancoris/appengine:${tag}
+docker push ancoris/appengine:${tag}
 
 docker build appengine-with-python-libraries --no-cache --tag ancoris/appengine-with-python-libraries
-docker tag ancoris/appengine-with-python-libraries ancoris/appengine-with-python-libraries:${timestamp}
-docker push ancoris/appengine-with-python-libraries:${timestamp}
+docker tag ancoris/appengine-with-python-libraries ancoris/appengine-with-python-libraries:${tag}
+docker push ancoris/appengine-with-python-libraries:${tag}
 
-echo Tag is ${timestamp}
+echo Tag is ${tag}
